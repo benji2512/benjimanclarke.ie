2165b9b2ac8cd5012a6bf0faf52727688070a92e
Ben Clarke <ben.clarke1@ibm.com>
2026-03-02 16:12:27 +0000

    Working blog structure, display and first stab at writing

--- app.py
+++ app.py
@@ -1,30 +1,49 @@
-from flask  import Flask, render_template
+from flask import Flask, abort, render_template, request, url_for
+
+from blogs import get_post, get_posts
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "your secret key"
+
 
 @app.route("/helloworld")
 def helloWorld():
     return "<h1>Hello Ben</h1>"
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/aboutben")
 def aboutben():
     return render_template("aboutme.html")
 
+
 @app.route("/career")
 def career():
     return render_template("career.html")
+
 
 @app.route("/contact")
 def contact():
     return render_template("findme.html")
 
+
 @app.route("/blog")
 def blog():
-    return render_template("blog.html")
+    posts = get_posts()
+    return render_template("blog.html", posts=posts)
+
+
+@app.route("/blog/<slug>/")
+def blog_detail(slug: str):
+    post = get_post(slug)
+    if not post:
+        abort(404)
+    return render_template("post.html", post=post)
+
 
 @app.route("/projects")
 def projects():
